2718d2996695c0d189f923a25213c3b0dad468fc
marimo <99727726+marimarimo0228@users.noreply.github.com>
2026-01-19 21:44:01 +0900

    Update app.py

--- app.py
+++ app.py
@@ -42,4 +42,11 @@ def add_pokemon():
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
+
     app.run(debug=True)
+
+if __name__ == '__main__':
+    # サーバー上ではポート番号などを環境に合わせる必要があるため
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
